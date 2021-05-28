ba98df77d84038d221d978d7e05f48e6a7027536
Mohan H N <>
2021-05-28 11:44:21 +0000

    Done

--- Program_1.c
+++ Program_1.c
@@ -5,9 +5,20 @@
 int main()
 {
 	char str[10];
+  int i=0,no=0;
+  printf("Enter the string: ");
 	scanf("%s", str);
-
 	//Write your code here
-
+  while(str[i++]!='\0')
+  {
+    if(str[i]=='a'||
+      str[i]=='e'||
+      str[i]=='i'||
+      str[i]=='o'||
+      str[i]=='u'||
+      str[i]=='\0')
+      no++;
+  }
+  printf("No of vowels = %d",no);
 	return 0;
 }
