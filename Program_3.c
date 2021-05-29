ec1d3b14a6519e13a9cdfc98f6c31f84071757df
Mohan H N <>
2021-05-29 08:10:33 +0000

    Completed

--- Program_3.c
+++ Program_3.c
@@ -6,12 +6,26 @@
 //output:
 //Palindrome
 #include<stdio.h>
-
+#include<string.h>
 int main()
 {
-	char str[10];
+	char str[10],temp;
+  int i=0,j=0;
+  printf("Enter the string : ");
 	scanf("%s", str);
-
+  j=strlen(str)-1;
+  while(i<j)
+  {
+  temp=str[i];
+  str[i]=str[j];
+  str[j]=temp;
+  i++;
+  j--;
+  }
+  if(strcmp(str[i]==str[j]))
+   printf("yes");
+  else
+   printf("No");
 	//Write your code here
 	
 	return 0;
